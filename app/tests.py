363cd7b3f0ec8b44e593c2e16b5620d71c1394d3
Michael Herman <hermanmu@gmail.com>
2014-09-30 08:57:45 -0700

    updated unit tests

--- app/tests.py
+++ app/tests.py
@@ -8,6 +8,7 @@ class MainTestCase(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/data')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
 
     def test_index(self):
         tester = app.test_client(self)
